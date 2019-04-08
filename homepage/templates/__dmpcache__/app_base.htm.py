02f577228143c98f622cedfd8612cb105f45088c
sconnole <seanconnole32@gmail.com>
2019-04-08 10:27:18 -0600

    Added user file

--- homepage/templates/__dmpcache__/app_base.htm.py
+++ homepage/templates/__dmpcache__/app_base.htm.py
@@ -5,14 +5,14 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1554740435.677435
+_modified_time = 1554740435.7233157
 _enable_loop = True
-_template_filename = 'C:/Users/stick/Developer/Intex/homepage/templates/index.html'
-_template_uri = 'index.html'
+_template_filename = 'C:/Users/stick/Developer/Intex/homepage/templates/app_base.htm'
+_template_uri = 'app_base.htm'
 _source_encoding = 'utf-8'
 import django_mako_plus
 import django.utils.html
-_exports = ['title', 'site_center']
+_exports = ['title']
 
 
 def _mako_get_namespace(context, name):
@@ -25,24 +25,17 @@ def _mako_generate_namespaces(context):
     pass
 def _mako_inherit(template, context):
     _mako_generate_namespaces(context)
-    return runtime._inherit_from(context, 'app_base.htm', _template_uri)
+    return runtime._inherit_from(context, 'base.htm', _template_uri)
 def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
         def title():
             return render_title(context._locals(__M_locals))
-        def site_center():
-            return render_site_center(context._locals(__M_locals))
         __M_writer = context.writer()
-        __M_writer('\r\n\r\n')
+        __M_writer('\r\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'title'):
             context['self'].title(**pageargs)
-        
-
-        __M_writer('\r\n')
-        if 'parent' not in context._data or not hasattr(context._data['parent'], 'site_center'):
-            context['self'].site_center(**pageargs)
         
 
         __M_writer('\r\n')
@@ -63,20 +56,8 @@ def render_title(context,**pageargs):
         context.caller_stack._pop_frame()
 
 
-def render_site_center(context,**pageargs):
-    __M_caller = context.caller_stack._push_frame()
-    try:
-        def site_center():
-            return render_site_center(context)
-        __M_writer = context.writer()
-        __M_writer('\r\n    <div class="content">\r\n      <p>Sit reprehenderit ad elit nostrud.</p>\r\n    </div>\r\n')
-        return ''
-    finally:
-        context.caller_stack._pop_frame()
-
-
 """
 __M_BEGIN_METADATA
-{"filename": "C:/Users/stick/Developer/Intex/homepage/templates/index.html", "uri": "index.html", "source_encoding": "utf-8", "line_map": {"29": 0, "38": 1, "43": 3, "48": 8, "54": 3, "60": 3, "66": 4, "72": 4, "78": 72}}
+{"filename": "C:/Users/stick/Developer/Intex/homepage/templates/app_base.htm", "uri": "app_base.htm", "source_encoding": "utf-8", "line_map": {"29": 0, "36": 1, "41": 2, "47": 2, "53": 2, "59": 53}}
 __M_END_METADATA
 """

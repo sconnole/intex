5f5c90c05759d2824ba34ca3267b509f090698fc
sconnole <seanconnole32@gmail.com>
2019-04-08 15:05:52 -0600

    Added sql connection

--- homepage/views/index.py
+++ homepage/views/index.py
@@ -1,14 +1,28 @@
 from django.conf import settings
 from django_mako_plus import view_function, jscontext
-from datetime import datetime, timezone
 
 @view_function
 def process_request(request):
-    utc_time = datetime.utcnow()
-    context = {
-        # sent to index.html:
-        'utc_time': utc_time,
-        # sent to index.html and index.js:
-        jscontext('utc_epoch'): utc_time.timestamp(),
-    }
-    return request.dmp.render('index.html', context)
+    import pyodbc
+    cs = (
+    "DRIVER={ODBC Driver 13 for SQL Server};"
+    "SERVER=tcp:eligardner.database.windows.net,1433;"
+    "Datbase=INTEXII;"
+    "UID=eligardner@eligardner;"
+    "PWD=Password413"
+    )
+    print(cs)
+
+    conn = pyodbc.connect(cs)
+    print("connected")
+    
+    cursor = conn.cursor()
+    sql = "SELECT * FROM dbo.triple"
+
+    cursor.execute(sql)
+
+    for row in cursor:
+        print(row)
+
+
+    return request.dmp.render('index.html')

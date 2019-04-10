3ac78796e3ca4eac812d8dcfdff499312b956671
sconnole <seanconnole32@gmail.com>
2019-04-10 17:48:46 -0600

    Added admin search

--- account/templates/__dmpcache__/account/templates/admin.html.py
+++ account/templates/__dmpcache__/account/templates/admin.html.py
@@ -5,14 +5,14 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1554925640.9696114
+_modified_time = 1554939982.7221813
 _enable_loop = True
 _template_filename = 'C:/Users/stick/Developer/Intex/account/templates/admin.html'
 _template_uri = '/account/templates/admin.html'
 _source_encoding = 'utf-8'
 import django_mako_plus
 import django.utils.html
-_exports = ['site_right']
+_exports = ['site_center', 'site_right']
 
 
 def _mako_get_namespace(context, name):
@@ -30,17 +30,58 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        self = context.get('self', UNDEFINED)
         form = context.get('form', UNDEFINED)
+        docs = context.get('docs', UNDEFINED)
+        self = context.get('self', UNDEFINED)
+        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
+        def site_center():
+            return render_site_center(context._locals(__M_locals))
         def site_right():
             return render_site_right(context._locals(__M_locals))
         __M_writer = context.writer()
+        __M_writer('\r\n\r\n')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'site_center'):
+            context['self'].site_center(**pageargs)
+        
+
         __M_writer('\r\n\r\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'site_right'):
             context['self'].site_right(**pageargs)
         
 
         __M_writer('\r\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_site_center(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        docs = context.get('docs', UNDEFINED)
+        self = context.get('self', UNDEFINED)
+        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
+        def site_center():
+            return render_site_center(context)
+        __M_writer = context.writer()
+        __M_writer('\r\n     <div class="content">\r\n        <div class="prescribers">\r\n            <table>\r\n                <tr>\r\n                    <th><img src="')
+        __M_writer(django_mako_plus.ExpressionPostProcessor(self)(STATIC_URL))
+        __M_writer('homepage/media/user.png" alt="user"></th>\r\n                    <th>First Name</th>\r\n                    <th>Last Name</th>\r\n                    <th></th>\r\n                    <th></th>\r\n                    <th></th>\r\n                </tr>\r\n')
+        for doc in docs: 
+            __M_writer('                    <tr>\r\n                        <td><img src="')
+            __M_writer(django_mako_plus.ExpressionPostProcessor(self)(STATIC_URL))
+            __M_writer('homepage/media/user.png" alt="user"></td>\r\n                        <td>')
+            __M_writer(django_mako_plus.ExpressionPostProcessor(self)(doc[0]))
+            __M_writer('</td>\r\n                        <td>')
+            __M_writer(django_mako_plus.ExpressionPostProcessor(self)(doc[1]))
+            __M_writer('</td>\r\n                        <td><a class="page-btn" href="/account/edit/')
+            __M_writer(django_mako_plus.ExpressionPostProcessor(self)(doc[2]))
+            __M_writer('/">Edit</a></td>\r\n                        <td><a class="page-btn" href="/account/delete/')
+            __M_writer(django_mako_plus.ExpressionPostProcessor(self)(doc[2]))
+            __M_writer('/">Delete</a></td>\r\n                        <td><a class="page-btn" href="/account/add_drug/')
+            __M_writer(django_mako_plus.ExpressionPostProcessor(self)(doc[2]))
+            __M_writer('/">Add Drug</a></td>\r\n                    </tr>\r\n')
+        __M_writer('            </table>\r\n        </div>\r\n    </div>\r\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -64,6 +105,6 @@ def render_site_right(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "C:/Users/stick/Developer/Intex/account/templates/admin.html", "uri": "/account/templates/admin.html", "source_encoding": "utf-8", "line_map": {"29": 0, "38": 1, "43": 13, "49": 3, "57": 3, "58": 6, "59": 6, "65": 59}}
+{"filename": "C:/Users/stick/Developer/Intex/account/templates/admin.html", "uri": "/account/templates/admin.html", "source_encoding": "utf-8", "line_map": {"29": 0, "42": 1, "47": 28, "52": 40, "58": 3, "67": 3, "68": 8, "69": 8, "70": 15, "71": 16, "72": 17, "73": 17, "74": 18, "75": 18, "76": 19, "77": 19, "78": 20, "79": 20, "80": 21, "81": 21, "82": 22, "83": 22, "84": 25, "90": 30, "98": 30, "99": 33, "100": 33, "106": 100}}
 __M_END_METADATA
 """
--- account/views/admin.py
+++ account/views/admin.py
@@ -2,19 +2,49 @@ from django.conf import settings
 from django_mako_plus import view_function
 from homepage.views.index import SearchForm
 from django.http import HttpResponseRedirect
+import pyodbc
 
-ROWS = 5
+ROWS = 10
 OFFSET = 0
 
 @view_function
-def process_request(request):
+def process_request(request, page:int=0):
     if not request.user.has_perm('account.add_user'):
         return HttpResponseRedirect('/account/permission_denied/') 
 
-    
+    OFFSET = page * ROWS
 
+    if request.method == "POST": 
+        request.session['admin_search'] = convertParam(request.POST['search'])
+    
+    if 'admin_search' in request.session:
+        param = request.session['admin_search'] 
+    else:
+        param = ""
+
+    param = convertParam(param)
+    sql = ('''SELECT FName, LName, DoctorID  
+            FROM dbo.prescriber 
+            WHERE Lname LIKE ? 
+            OR Fname LIKE ? 
+            ORDER BY LName
+            OFFSET ? ROWS
+            FETCH NEXT ? ROWS ONLY
+    ;''')
+
+    docs = dSQL(sql, param, OFFSET)
+    
     context = {
-        "form": SearchForm()
+        "form": SearchForm(),
+        "docs": docs
     }
 
     return request.dmp.render('/account/templates/admin.html', context)
+
+def convertParam (param):
+    return "%" + param + "%"
+
+def dSQL (sql, param, offset):
+    conn = pyodbc.connect(settings.CONNECTION_STRING)
+    cursor = conn.cursor()
+    return cursor.execute(sql, (param, param, offset, ROWS))

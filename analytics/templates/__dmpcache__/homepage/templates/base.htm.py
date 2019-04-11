c32def2efa16e26d5580673dfe84b68fc4802dd0
sconnole <seanconnole32@gmail.com>
2019-04-11 16:19:49 -0600

    Updated links

--- analytics/templates/__dmpcache__/homepage/templates/base.htm.py
+++ analytics/templates/__dmpcache__/homepage/templates/base.htm.py
@@ -5,9 +5,9 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1555011735.1265957
+_modified_time = 1555021018.2675135
 _enable_loop = True
-_template_filename = 'C:/Users/majohnso/Desktop/intex/homepage/templates/base.htm'
+_template_filename = 'C:/Users/stick/Developer/Intex/homepage/templates/base.htm'
 _template_uri = 'homepage/templates/base.htm'
 _source_encoding = 'utf-8'
 import django_mako_plus
@@ -19,18 +19,18 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        request = context.get('request', UNDEFINED)
-        self = context.get('self', UNDEFINED)
-        user = context.get('user', UNDEFINED)
+        def menu():
+            return render_menu(context._locals(__M_locals))
         def site_center():
             return render_site_center(context._locals(__M_locals))
+        request = context.get('request', UNDEFINED)
+        self = context.get('self', UNDEFINED)
         def site_right():
             return render_site_right(context._locals(__M_locals))
         STATIC_URL = context.get('STATIC_URL', UNDEFINED)
         def title():
             return render_title(context._locals(__M_locals))
-        def menu():
-            return render_menu(context._locals(__M_locals))
+        user = context.get('user', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\r\n<!DOCTYPE html>\r\n<html>\r\n    <meta charset="UTF-8">\r\n    <head>\r\n        <link rel="icon" href="')
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)(STATIC_URL))
@@ -51,8 +51,7 @@ def render_body(context,**pageargs):
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)( django_mako_plus.links(self) ))
         __M_writer(' \r\n    </head>\r\n    <body>\r\n        <header id="header">\r\n            <nav class="navbar navbar-expand-lg">\r\n                <a href="/"><img src="')
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)( STATIC_URL ))
-        __M_writer('homepage/media/logo.png" alt="home" /></a>\r\n')
-        __M_writer('                ')
+        __M_writer('homepage/media/logo.png" alt="home" /></a>\r\n                ')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'menu'):
             context['self'].menu(**pageargs)
         
@@ -89,13 +88,18 @@ def render_menu(context,**pageargs):
     try:
         def menu():
             return render_menu(context)
-        self = context.get('self', UNDEFINED)
         request = context.get('request', UNDEFINED)
+        self = context.get('self', UNDEFINED)
         user = context.get('user', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\r\n                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">\r\n                        <span class="icon-bar"></span>\r\n                        <span class="icon-bar"></span>\r\n                        <span class="icon-bar"></span> \r\n                    </button>\r\n                    <div class="dropdown collapse navbar-collapse" id="myNavbar">        \r\n                        <ul class="nav navbar-nav">\r\n                            <li><a class="dropdown-item nav-link ')
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)('active' if request.dmp.page == 'index' else ''))
-        __M_writer('" href="/">Home</a></li>\r\n                        </ul>\r\n                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">\r\n                            Welcome')
+        __M_writer('" href="/">Home</a></li>\r\n')
+        if user.has_perm('account.delete_user'):
+            __M_writer('                                <li><a class="dropdown-item nav-link ')
+            __M_writer(django_mako_plus.ExpressionPostProcessor(self)('active' if request.dmp.page == 'index' else ''))
+            __M_writer('" href="/account/admin/">Admin</a></li>\r\n')
+        __M_writer('                        </ul>\r\n                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">\r\n                            Welcome')
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)(',' if user.username != '' else ''))
         __M_writer(' ')
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)(user.username))
@@ -104,7 +108,10 @@ def render_menu(context,**pageargs):
             __M_writer('                                <a class="dropdown-item nav-link" href="/account/logout">Logout</a>\r\n                                <a class="dropdown-item nav-link" href="/account/preferences">Account</a>\r\n')
         else: 
             __M_writer('                                <a class="dropdown-item nav-link" href="/account/login">Login</a>\r\n')
-        __M_writer('                        </div>\r\n                    </div>        \r\n                ')
+        __M_writer('                        </div>\r\n                    </div> \r\n')
+        if user.has_perm('account.view_analytics'): 
+            __M_writer('                    <div class="dropdown2 collapse navbar-collapse" id="">\r\n                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownAnalytics" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">\r\n                            Analytics\r\n                        </button>\r\n                        <div class="dropdown-menu" aria-labelledby="dropdownAnalytics">\r\n                            <a class="dropdown-item nav-link" href="/analytics/index/">Unethical</a>\r\n                            <a class="dropdown-item nav-link" href="/analytics/opiateprescriber/">Prescribers</a>\r\n                            <a class="dropdown-item nav-link" href="/analytics/risk/">At-Risk</a>\r\n                        </div>   \r\n                    </div> \r\n')
+        __M_writer('\r\n                ')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -136,6 +143,6 @@ def render_site_right(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "C:/Users/majohnso/Desktop/intex/homepage/templates/base.htm", "uri": "homepage/templates/base.htm", "source_encoding": "utf-8", "line_map": {"18": 0, "35": 2, "36": 7, "37": 7, "42": 8, "43": 11, "44": 11, "45": 11, "46": 12, "47": 12, "48": 13, "49": 13, "50": 16, "51": 17, "52": 17, "53": 22, "54": 22, "55": 24, "60": 46, "65": 54, "70": 59, "76": 8, "87": 24, "96": 24, "97": 32, "98": 32, "99": 35, "100": 35, "101": 35, "102": 35, "103": 38, "104": 39, "105": 41, "106": 42, "107": 44, "113": 52, "119": 52, "125": 57, "131": 57, "137": 131}}
+{"filename": "C:/Users/stick/Developer/Intex/homepage/templates/base.htm", "uri": "homepage/templates/base.htm", "source_encoding": "utf-8", "line_map": {"18": 0, "35": 2, "36": 7, "37": 7, "42": 8, "43": 11, "44": 11, "45": 11, "46": 12, "47": 12, "48": 13, "49": 13, "50": 16, "51": 17, "52": 17, "53": 22, "54": 22, "59": 61, "64": 69, "69": 74, "75": 8, "86": 23, "95": 23, "96": 31, "97": 31, "98": 32, "99": 33, "100": 33, "101": 33, "102": 35, "103": 37, "104": 37, "105": 37, "106": 37, "107": 40, "108": 41, "109": 43, "110": 44, "111": 46, "112": 48, "113": 49, "114": 60, "120": 67, "126": 67, "132": 72, "138": 72, "144": 138}}
 __M_END_METADATA
 """
--- analytics/templates/__dmpcache__/index.html.py
+++ analytics/templates/__dmpcache__/index.html.py
@@ -5,9 +5,9 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1555011366.2619786
+_modified_time = 1555021112.997284
 _enable_loop = True
-_template_filename = 'C:/Users/majohnso/Desktop/intex/analytics/templates/index.html'
+_template_filename = 'C:/Users/stick/Developer/Intex/analytics/templates/index.html'
 _template_uri = 'index.html'
 _source_encoding = 'utf-8'
 import django_mako_plus
@@ -30,13 +30,13 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        related = context.get('related', UNDEFINED)
-        page = context.get('page', UNDEFINED)
         dropdown = context.get('dropdown', UNDEFINED)
-        self = context.get('self', UNDEFINED)
-        prescribers = context.get('prescribers', UNDEFINED)
         def site_center():
             return render_site_center(context._locals(__M_locals))
+        page = context.get('page', UNDEFINED)
+        prescribers = context.get('prescribers', UNDEFINED)
+        self = context.get('self', UNDEFINED)
+        related = context.get('related', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\r\n\r\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'site_center'):
@@ -52,13 +52,13 @@ def render_body(context,**pageargs):
 def render_site_center(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        related = context.get('related', UNDEFINED)
-        page = context.get('page', UNDEFINED)
         dropdown = context.get('dropdown', UNDEFINED)
-        self = context.get('self', UNDEFINED)
-        prescribers = context.get('prescribers', UNDEFINED)
         def site_center():
             return render_site_center(context)
+        page = context.get('page', UNDEFINED)
+        prescribers = context.get('prescribers', UNDEFINED)
+        self = context.get('self', UNDEFINED)
+        related = context.get('related', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\r\n    \r\n<h1>Unethical Perscribers: </h1>\r\n\r\n<div class="prescribers">\r\n    <table>\r\n        <tr>\r\n            <th>Name</th>\r\n            <th>Specialty</th>\r\n            <th>Opioid perscription ratio</th>\r\n        </tr>\r\n')
         for doc in prescribers: 
@@ -71,7 +71,7 @@ def render_site_center(context,**pageargs):
             __M_writer('</td>\r\n            </tr>\r\n')
         __M_writer('    </table>\r\n    <div class="paginate">\r\n        <a class="page-btn ')
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)('hide' if page == 0 else ''))
-        __M_writer('" href="/analytics/index">Prev</a>\r\n        <a class="page-btn right" href="/analytics/index">More</a>\r\n    </div> \r\n\r\n   \r\n</div>\r\n\r\n<div> \r\n    <br>\r\n    <br>\r\n    <form action = "index" method = "POST"> \r\n    <h2>Find similar potential Unethical Perscribers: </h2>\r\n    <select name ="doctorID">\r\n')
+        __M_writer('" href="/analytics/index">Prev</a>\r\n        <a class="page-btn right" href="/analytics/index">More</a>\r\n    </div> \r\n\r\n   \r\n</div>\r\n\r\n<div> \r\n    <br>\r\n    <br>\r\n    <form method = "POST"> \r\n    <h2>Find similar potential Unethical Perscribers: </h2>\r\n    <select name ="doctorID">\r\n')
         for item in dropdown: 
             __M_writer('                <option value=')
             __M_writer(django_mako_plus.ExpressionPostProcessor(self)(item[1]))
@@ -91,6 +91,6 @@ def render_site_center(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "C:/Users/majohnso/Desktop/intex/analytics/templates/index.html", "uri": "index.html", "source_encoding": "utf-8", "line_map": {"29": 0, "41": 1, "46": 59, "52": 3, "63": 3, "64": 15, "65": 16, "66": 17, "67": 17, "68": 18, "69": 18, "70": 19, "71": 19, "72": 22, "73": 24, "74": 24, "75": 37, "76": 38, "77": 38, "78": 38, "79": 38, "80": 38, "81": 40, "82": 48, "83": 49, "84": 49, "85": 49, "86": 51, "92": 86}}
+{"filename": "C:/Users/stick/Developer/Intex/analytics/templates/index.html", "uri": "index.html", "source_encoding": "utf-8", "line_map": {"29": 0, "41": 1, "46": 59, "52": 3, "63": 3, "64": 15, "65": 16, "66": 17, "67": 17, "68": 18, "69": 18, "70": 19, "71": 19, "72": 22, "73": 24, "74": 24, "75": 37, "76": 38, "77": 38, "78": 38, "79": 38, "80": 38, "81": 40, "82": 48, "83": 49, "84": 49, "85": 49, "86": 51, "92": 86}}
 __M_END_METADATA
 """

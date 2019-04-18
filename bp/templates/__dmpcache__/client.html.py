05a54a6455505e0c9751a162278ca2be37bc4030
sconnole <seanconnole32@gmail.com>
2019-04-17 18:49:30 -0600

    Added template form for client.py

--- bp/templates/__dmpcache__/client.html.py
+++ bp/templates/__dmpcache__/client.html.py
@@ -5,14 +5,14 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1555516687.4000885
+_modified_time = 1555548000.065495
 _enable_loop = True
 _template_filename = 'C:/Users/stick/Developer/Intex/bp/templates/client.html'
 _template_uri = 'client.html'
 _source_encoding = 'utf-8'
 import django_mako_plus
 import django.utils.html
-_exports = []
+_exports = ['content']
 
 
 def _mako_get_namespace(context, name):
@@ -30,7 +30,32 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
+        def content():
+            return render_content(context._locals(__M_locals))
+        form = context.get('form', UNDEFINED)
+        self = context.get('self', UNDEFINED)
         __M_writer = context.writer()
+        __M_writer('\r\n\r\n')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
+            context['self'].content(**pageargs)
+        
+
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_content(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def content():
+            return render_content(context)
+        form = context.get('form', UNDEFINED)
+        self = context.get('self', UNDEFINED)
+        __M_writer = context.writer()
+        __M_writer('\r\n    <div class="form-content">\r\n        <form method="post">\r\n            <table>\r\n            ')
+        __M_writer(django_mako_plus.ExpressionPostProcessor(self)( form.as_p() ))
+        __M_writer('\r\n            </table>\r\n            <input class="main-btn" type="submit" value="Predict">\r\n        </form>\r\n    </div>\r\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -38,6 +63,6 @@ def render_body(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "C:/Users/stick/Developer/Intex/bp/templates/client.html", "uri": "client.html", "source_encoding": "utf-8", "line_map": {"29": 0, "39": 29}}
+{"filename": "C:/Users/stick/Developer/Intex/bp/templates/client.html", "uri": "client.html", "source_encoding": "utf-8", "line_map": {"29": 0, "38": 1, "48": 3, "56": 3, "57": 7, "58": 7, "64": 58}}
 __M_END_METADATA
 """
--- bp/views/client.py
+++ bp/views/client.py
@@ -1,12 +1,77 @@
 from django.conf import settings
 from django_mako_plus import view_function, jscontext
 from datetime import datetime, timezone
+from django import forms
+from django.core.exceptions import ValidationError
 
 @view_function
 def process_request(request):
+    request.session.flush() 
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
 
+        if form.is_valid():
+            user = authenticate(request, username=form["username"].value(), password=form["password"].value())
+            login(request, user)
+            return HttpResponseRedirect('/bp/index/')
+        else:
+            pass
+    else:
+        form = ClientForm()
+    
     context = {
-        
+        'form': form,
     }
-   
+
     return request.dmp.render('client.html', context)
+
+class ClientForm(forms.Form):    
+    
+    CHOICES = (('Option 1', 'Option 1'),('Option 2', 'Option 2'),)
+
+    LeadSource = forms.ChoiceField(
+                label="Lead Source",
+                choices=CHOICES,
+                widget=forms.Select(attrs={'class': 'req'})
+                )
+    RepliesFromClient = forms.IntegerField(
+                label="Replies From Client",
+                initial=0,
+                min_value=0,
+                widget=forms.NumberInput(attrs={'class': 'req'})
+                )
+    EmailsToClient = forms.IntegerField(
+                label="Replies From Client",
+                initial=0,
+                min_value=0,
+                widget=forms.NumberInput(attrs={'class': 'req'})
+                )
+    TotalTickets = forms.IntegerField(
+                label="Total Number of Tickets",
+                initial=0,
+                min_value=0,
+                widget=forms.NumberInput(attrs={'class': 'req'})
+                )
+    DaysAsClient = forms.IntegerField(
+                label="Days as a Client",
+                initial=0,
+                min_value=0,
+                widget=forms.NumberInput(attrs={'class': 'req'})
+                )
+    Num_Locations = forms.IntegerField(
+                label="Number of Locations",
+                initial=0,
+                min_value=0,
+                widget=forms.NumberInput(attrs={'class': 'req'})
+                )
+    FB = forms.ChoiceField(
+                choices=(("1", "Yes"), ("0", "No"),),
+                widget=forms.Select(attrs={'class': 'req'})
+                )
+    PE_Videos = forms.ChoiceField(
+                choices=(("1", "Yes"), ("0", "No"),),
+                widget=forms.Select(attrs={'class': 'req'})                
+                )
+    
+    # SEONumeric
+
